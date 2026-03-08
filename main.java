2a930008738aae450fa5e1f4ad497847bd755ef1
adityak5212 <adityak5212@gmail.com>
2026-03-08 19:33:50 +0530

    Add files via upload

--- main.java
+++ main.java
@@ -1,18 +1,34 @@
 public class main {
 
     public static void main(String[] args){
+        int age = 21;
+        int year = 2026;
+        double price = 17672;
+        char currency = '$';
+         boolean isStudent = true;
+         boolean forSale = false;
 
-       int year = 2026;
-       double price = 16.73;
-       char currency = '$';
-       boolean isStudent = true;
-       boolean forSale = false;
+        String name = "Aditya";
+        String food = "daal chawal";
+        String email = "adtyak5212@gmail.com";
+        String car = "mustang";
+        String color = "blue";
 
-        System.out.println("this year is " + year);
-        System.out.println("$" + price);
-        System.out.println(currency);
-        System.out.println(isStudent);
-        System.out.println(forSale);
+        System.out.println("Hello " +name);
+        System.out.println("your favorite food is: " + food);
+        System.out.println("your email is " + email);
+        System.out.println("your choice is " + color + " " +year+ " " +car );
+        System.out.println("the price is: " +currency + " " +price);
+
+        if(forSale) {
+            System.out.println("there is a " + car + " for sale");
+        }
+        else{
+            System.out.println("the " + car + "is not for sale");
+        }
+
+
+
 
     }
 }
