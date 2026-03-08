bf3c827d9723717ee7d4a13e1a14bdb1d19a90a4
adityak5212 <adityak5212@gmail.com>
2026-03-08 13:01:48 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,17 @@
+public class main {
+
+    public static void main(String[] args){
+
+        // THIS IS MY FIRST PROGRAM
+
+        /*
+           this is a multi-line comment
+         */
+
+        System.out.println("i will make more projects!");
+        System.out.println("i am surprised to learn all of this");
+        System.out.println("i am going to keep learning.");
+
+
+    }
+}
