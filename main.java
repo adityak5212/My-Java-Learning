8a63e7c9c2cd965df3615c2aaf8cf1eae502a489
adityak5212 <adityak5212@gmail.com>
2026-03-12 12:56:07 +0530

    if statement = performs a block of code if its condition is true

--- main.java
+++ main.java
@@ -1,25 +1,48 @@
 import java.util.Scanner;
-public class main{
-    public static void main(String[] args){
+public class main {
+    public static void main (String[] args) {
 
-        // Calculate area of a rectangle
+        // if statement =  performs  block of code if its condition is true
 
-        double width = 0;
-        double height = 0;
-        double area = 0;
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner (System.in);
+        String name;
+        int age = 50;
+        boolean isStudent;
+        System.out.println("enter your name: 😊");
+        name = scanner.nextLine();
+        System.out.println("enter your age: 😒");
+        age = scanner.nextInt();
+        System.out.println("are you a student (true /false): 👍");
+        isStudent = scanner.nextBoolean();
+        if(name.isEmpty()){
+            System.out.println("you didn't enter your name 🙌");
+        }
+        else{
+            System.out.println("hello " + name + "!");
+        }
 
-        System.out.println("enter the width");
-        width = scanner.nextDouble();
-
-        System.out.println("enter the height: ");
-        height = scanner.nextDouble();
-
-        area = width * height;
-        System.out.println("the area is: " + area + "cm>");
-
+        if(age >= 65){
+            System.out.println("you are a senior! 👴");
+        }
+        else if(age >=18){
+            System.out.println("you are an adult! 🧑‍🦰");
+        }
+        else if(age > 0){
+            System.out.println("you haven't been born yet! ❤️");
+        }
+        else if (age== 0) {
+            System.out.println("you are a baby! 👶");
+        }
+        else{
+            System.out.println("you are a child! 👼");
+        }
+        if(isStudent){
+            System.out.println("you are a student! 🧑‍🎓");
+        }
+        else{
+            System.out.println("you are not a student 🧑‍🎓");
+        }
         scanner.close();
-
 
     }
 }
