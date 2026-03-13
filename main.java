a505441d60eea745baa81126802769da1d3d281e
adityak5212 <adityak5212@gmail.com>
2026-03-14 00:09:43 +0530

    Printf

--- main.java
+++ main.java
@@ -1,30 +1,29 @@
-import java.util.Scanner;
+
 public class main{
     public static void main(String[] argc){
 
-        // circumference = 2* Math.PI * radius;
-        // area = Math.PI * Math.pow(radius, 2);
-        // volume = (4.0/ 3.0) * Math.pow(radius,3)
+        // printf() = is a method used to format outou
+        // %[flags] [width][.precision] [specifier-character]
 
-        Scanner scanner = new Scanner (System.in);
+        String name = "Ayush";
+        char firstLetter = 'A';
+        int age = 22;
+        double height = 178.5;
+        boolean isEmployed = true;
 
-        double radius;
-        double circumference;
-        double area;
-        double volume;
+        System.out.printf("Hello %s\n", name);
+        System.out.printf("your name start with a %c\n", firstLetter);
+        System.out.printf("you are %d yers old\n", age);
+        System.out.printf("you are %fcm tall\n", height);
+        System.out.printf("Employed: %b\n", isEmployed);
 
-        System.out.println("Enter the radius: ");
-        radius = scanner.nextDouble();
+        double price1 = 27.3;
+        double price2 =32;
+        double price3 =23.3;
 
-        circumference = 2* Math.PI * radius;
-        area = Math.PI * Math.pow(radius, 2);
-        volume = (4.0/ 3.0) * Math.pow(radius,3);
-
-        System.out.println("the circumference is: " + circumference + "cm");
-        System.out.println("the area is: " + area + "cm^2");
-        System.out.println("the volume is: " + volume + "cm^3");
-
-        scanner.close();
+        System.out.printf("% 2f\n", price1);
+        System.out.printf("% 2f\n", price2);
+        System.out.printf("2%f\n", price3);
 
 
 
