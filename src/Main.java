76f33e9defd158aa73c337d5d41b59b54e47459d
adityak5212 <adityak5212@gmail.com>
2026-03-28 11:09:24 +0530

    while loop = repeat some forever   while some condition remains true

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+
+        //while loop = repeat some forever
+        //      while some condition remains true
+
+        Scanner scanner = new Scanner(System.in);
+
+        String name = "";
+
+        while(name.isEmpty()){
+            System.out.print("Enter your name: ");
+            name = scanner.nextLine();
+        }
+
+        System.out.println("Hello " + name);
+
+
+        int number = 0;
+        do{
+            System.out.println("Enter a number between 1 - 10: ");
+            number = scanner.nextInt();
+        } while(number < 1 || number > 10);
+        System.out.println("You picked " + number);
+        scanner.close();
+
+    }
+}
